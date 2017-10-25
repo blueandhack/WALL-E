bb927029c1f9648cfaa2971fdefd2d6615de4e41
Yoga Lin <blueandhack@gmail.com>
2017-10-25 04:28:57 -0700

    add run.py

---
+++ run.py
@@ -0,0 +1,31 @@
+# import speech_recognition as sr
+#
+# # Record Audio
+# r = sr.Recognizer()
+# with sr.Microphone() as source:
+#     print("Say something!")
+#     audio = r.listen(source)
+#     print("stop")
+# # Speech recognition using Google Speech Recognition
+# try:
+#     # for testing purposes, we're just using the default API key
+#     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+#     # instead of `r.recognize_google(audio)`
+#     print("You said: " + r.recognize_google(audio))
+# except sr.UnknownValueError:
+#     print("Google Speech Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+import matplotlib.pyplot as plt
+fig,ax=plt.subplots()
+y1=[]
+for i in range(50):
+    y1.append(i)
+    ax.cla()
+    ax.bar(y1,label='test',height=y1,width=0.3)
+    ax.legend()
+    plt.pause(0.3)
+    print("try")
+
+print("Hello")
